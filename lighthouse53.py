9812a048b4aa1a5c6874beefa9a2c3c3ac7b59c3
Noah Kipin <nkipin@gmail.com>
2015-11-19 22:48:15 -0500

    Continued initial implementation
    Implemented most of getCurrentRecord; postIP

--- lighthouse53.py
+++ lighthouse53.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 """
-Lighthouse53
+lighthouse53
 
-Update Route53 domain 'A' level resources with your current external IP.
+UPSERT a Route53 dns resource record with your current external IPv4 address.
 
-By: Noah Kipin
-Date: 2015-11-18
+Noah Kipin <nkipin@gmail.com>
+2015-11-18
 """
 import logging
 import argparse
@@ -13,20 +13,41 @@ import urllib2
 import boto3
 
 def init():
+	parser = argparse.ArgumentParser(description='UPSERT a Route53 resource with your current external IPv4 address')
+	parser.add_argument('--loglevel')
+	parser.add_argument('--ttl')
+	parser.add_argument('hostedZoneId')
+	parser.add_argument('name')
+	#parser.add_argument('--type') need an external IPv6 API to do AAAA
 	logging.basicConfig(
-		filename='',
+		filename='/var/log/lighthouse53',
 		level=logging.DEBUG,
 		format='%(asctime)s %(message)s')
-	parser = argparse.ArgumentParser(description='UPSERT Route53 names')
-	parser.add_argument('--loglevel')
+	return parser.parse()
 
 def main():
 	currentIP = getExternalIP()
 	if (!currentIP):
 		logging.error('Unable to retrieve external IP')
+		return 100
+	client = boto3.client('route53')
+	currentReccord = getCurrentRecord(client, hostedZoneId, name)
+	if (!currentReccord):
+		logging.error('Unable to retrieve current record')
+		return 200
+	if(currentIP == currentReccord):
+		logging.info('No change')
+		return 0
+	else:
+		logging.info('')
 
-def getCurrentRecord():
-	pass
+def getCurrentRecord(client, hostedZoneId, name):
+	response = client.list_resource_record_sets(
+		HostedZoneId = hostedZoneId,
+		StartRecordName = name,
+		StartRecordType = 'A',
+		MaxItems='1')
+	return response['ResourceRecordSets'][0]['ResourceRecords'][0]['Value']
 
 def getExternalIP():
 	logging.debug('Retrieving external IP')
@@ -43,21 +64,32 @@ def getExternalIP():
 	else:
 		return False
 		
-def postIP(name, oldip, newip):
-	ChangeBatch = {
+def postIP(client, hostedZoneId, name, newip):
+	changeBatch = {
 		'Changes': [
 			{
 				'Action': 'UPSERT',
 				'ResourceRecordSet': {
 					'Name': name,
 					'Type': 'A',
-					'TTL': 2
+					'TTL': ttl,
+					'ResourceRecords': [
+						{
+							'Value': newip
+						}
+					]
 				}
 			}
 		]
 	}
-	pass
+	response = client.change_resource_record_sets(HostedZoneId = hostedZoneId, ChangeBatch = changeBatch)
+	loggin.debug('Change submitted')
+	if (response['Status'] == "PENDING"):
+		waiter = client.get_waiter('resource_record_sets_changed')
+		logging.debug('Change pending')
+		waiter.wait(Id=response['Id'])
+	logging.debug('Change insync')
 
 if __name__ == '__main__':
-	init()
-	main()
+	config = init()
+	return main(config)
