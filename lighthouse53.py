c5805fe572a3dc78285015c0b7a22e617326f8c3
Noah Kipin <nkipin@gmail.com>
2015-11-21 01:35:24 -0500

    Finished implementation

--- lighthouse53.py
+++ lighthouse53.py
@@ -13,48 +13,73 @@ import urllib2
 import boto3
 
 def init():
-	parser = argparse.ArgumentParser(description='UPSERT a Route53 resource with your current external IPv4 address')
-	parser.add_argument('--loglevel')
-	parser.add_argument('--ttl')
-	parser.add_argument('hostedZoneId')
-	parser.add_argument('name')
+	parser = argparse.ArgumentParser(
+		description='UPSERT a Route53 resource with your current external IPv4 address')
+	parser.add_argument('--loglevel', choices=['CRITICAL','INFO','WARN','ERROR','DEBUG'], type=str, help="The logging level you wish to see in the log file.", default='WARN')
+	parser.add_argument('--ttl', help="The time to live (ttl) that you wish to have assigned for the record",type=int,default=7200)
+	parser.add_argument('name', type=str, help="The domain record name to upsert")
+	parser.add_argument('hostedZoneId', type=str, help="The hostedZoneId in which this route53 domain is hosted")
 	#parser.add_argument('--type') need an external IPv6 API to do AAAA
+	config = parser.parse_args()
+	logLevels = {
+		'CRITICAL': 50,
+		'ERROR': 40,
+		'WARN': 30,
+		'INFO': 20,
+		'DEBUG': 10
+	}
 	logging.basicConfig(
 		filename='/var/log/lighthouse53',
-		level=logging.DEBUG,
+		level=logLevels[config.loglevel],
 		format='%(asctime)s %(message)s')
-	return parser.parse()
 
-def main():
+	return config
+
+def main(config):
 	currentIP = getExternalIP()
 	if (!currentIP):
 		logging.error('Unable to retrieve external IP')
-		return 100
+		return 1000
 	client = boto3.client('route53')
-	currentReccord = getCurrentRecord(client, hostedZoneId, name)
-	if (!currentReccord):
+	currentRecord = getCurrentRecord(client, config.hostedZoneId, config.name)
+	if (!currentRecord):
 		logging.error('Unable to retrieve current record')
-		return 200
-	if(currentIP == currentReccord):
+		return 1010
+	if(currentIP == currentRecord):
 		logging.info('No change')
 		return 0
 	else:
-		logging.info('')
+		if (postIP(client, config.hostedZoneId, config.name, currentIP, config.ttl)):
+			logging.info('Upsert with %s', currentIP)
+			return 0
+		else:
+			logging.warn('Upsert failed')
+			return 1020
 
 def getCurrentRecord(client, hostedZoneId, name):
-	response = client.list_resource_record_sets(
-		HostedZoneId = hostedZoneId,
-		StartRecordName = name,
-		StartRecordType = 'A',
-		MaxItems='1')
-	return response['ResourceRecordSets'][0]['ResourceRecords'][0]['Value']
+	logging.debug('Retrieving current record')
+	try:
+		response = client.list_resource_record_sets(
+			HostedZoneId = hostedZoneId,
+			StartRecordName = name,
+			StartRecordType = 'A',
+			MaxItems='1')
+	except:
+		logging.error('Error retrieving current record', exc_info = True)
+		return False
+	if ('ResourceRecordSets' not in response):
+		return False
+	else if ('ResourceRecords' not in response['ResourceRecordSets'][0]):
+		return False
+	else:
+		return response['ResourceRecordSets'][0]['ResourceRecords'][0]['Value']
 
 def getExternalIP():
 	logging.debug('Retrieving external IP')
 	try:
 		extIPResource = urllib2.urlopen('https://api.ipify.org')
-	except URLError:
-		logging.error(URLError)
+	except:
+		logging.error("Error retrieving external IP", exc_info = True)
 		return False
 	logging.debug("Status: %i", extIPResource.getcode())
 	if (extIPResource.getcode() == 200):
@@ -64,7 +89,8 @@ def getExternalIP():
 	else:
 		return False
 		
-def postIP(client, hostedZoneId, name, newip):
+def postIP(client, hostedZoneId, name, newip, ttl):
+	logging.debug('Changing resource record')
 	changeBatch = {
 		'Changes': [
 			{
@@ -82,13 +108,21 @@ def postIP(client, hostedZoneId, name, newip):
 			}
 		]
 	}
-	response = client.change_resource_record_sets(HostedZoneId = hostedZoneId, ChangeBatch = changeBatch)
-	loggin.debug('Change submitted')
+	try:
+		response = client.change_resource_record_sets(HostedZoneId = hostedZoneId, ChangeBatch = changeBatch)
+	except:
+		logging.error("Error submitting change request", exc_info = True)
+		return False
 	if (response['Status'] == "PENDING"):
-		waiter = client.get_waiter('resource_record_sets_changed')
 		logging.debug('Change pending')
-		waiter.wait(Id=response['Id'])
+		waiter = client.get_waiter('resource_record_sets_changed')
+		try:
+			waiter.wait(Id=response['Id'])
+		except:
+			logging.error("Error waiting for change", exc_info = True)
+			return False
 	logging.debug('Change insync')
+	return True
 
 if __name__ == '__main__':
 	config = init()
